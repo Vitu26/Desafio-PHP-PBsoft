275b80251282c44ea2714fd34566559ceddd9495
Vitu <loic.lemos@gmail.com>
2023-07-24 16:54:10 -0300

    terminado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,23 +7,30 @@ use App\Http\Requests\ProductStoreRequest;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Esse controlador permite que a aplicação realize operações CRUD (criar, ler, atualizar e excluir) para produtos e exiba a lista de produtos, permitindo que o usuário crie, edite, visualize e exclua produtos através das diferentes ações definidas no controlador.
+ */
+
 class HomeController extends Controller
 {
 
 
+    //método responsável por exibir a lista paginada de produtos na pagina inicial
     public function index()
     {
-        $products = Product::paginate(10);
-        return view('index', compact('products'));
+        $products = Product::paginate(10);//exibe 10 produtos pro pagina
+        return view('index', compact('products'));//retorna a view index e compacta os produtos retornados
     }
 
-
+    //método que exibe o formulario de criação
     public function create()
     {
         return view('create');
     }
 
 
+    //Processa a submissão do formulario de criação
+    //valida os dados recebidos usando a classe productstorerequest que contém as regras de validação
     public function store(ProductStoreRequest $request)
     {
         $register= Product::create([
@@ -34,6 +41,7 @@ class HomeController extends Controller
             'quanty' => $request->quanty,
 
         ]);
+        //se os dados forem válidos, cria um novo produto na tabela products e retorna a view
         if ($register) {
             return redirect('products');
         }
@@ -44,11 +52,12 @@ class HomeController extends Controller
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
+     * método responsável por exibir os detalhes de um produto em especifico com base no ID
      */
     public function show($id)
     {
-        $products= Product::find($id);
-        return view('show', compact('products'));
+        $products= Product::find($id);//procura o produto no banco de dados usando o ID
+        return view('show', compact('products'));//retorna a view e compacta os detalhes do produto
     }
 
     /**
@@ -56,14 +65,19 @@ class HomeController extends Controller
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
+     * Esse método exibe o formulário de edição de produto para um produto específico com base no ID fornecido.
      */
     public function edit($id)
     {
-        $products = Product::find($id);
-        return view('create', compact('products'));
+        $products = Product::find($id);//procura o produto no banco de dados com base no id
+        return view('create', compact('products'));//retorna a view 'create'(usada tanto pra criação quanto para edição)e compacta os detalhes do produto para serem usados na view.
     }
 
 
+
+    /**
+     * Esse método é responsável por processar a submissão do formulário de edição de produto.
+     */
     public function update(ProductStoreRequest $request, $id)
     {
         Product::where(['id' => $id])->update([
@@ -73,18 +87,23 @@ class HomeController extends Controller
             'value' => $request->value,
             'quanty' => $request->quanty,
         ]);
+        /**
+         * Ele valida os dados recebidos usando a classe ProductStoreRequest, que contém as regras de validação para a atualização de produtos.
+         * Se os dados forem válidos, ele atualiza o produto no banco de dados usando o modelo Product e o ID fornecido.
+         */
         return redirect('products');
     }
 
     /**
      * Remove the specified resource from storage.
+     * Esse método é responsável por excluir um produto com base no ID fornecido.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        Product::destroy($id);
-        return redirect('products');
+        Product::destroy($id);//Ele exclui o produto do banco de dados usando o modelo Product e o ID fornecido.
+        return redirect('products');//Após a exclusão do produto, ele redireciona o usuário para a página de lista de produtos.
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,13 @@ use App\Models\Product;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+/**
+ * O arquivo de rotas da web é usado para registrar as rotas relacionadas à interface da aplicação, permitindo que os usuários acessem diferentes páginas e recursos da aplicação por meio de URLs específicas. As rotas definidas nesse arquivo determinam quais controladores e ações do controlador serão responsáveis por processar as solicitações e retornar as views apropriadas para os usuários.
+ */
 
-Route::resource('/products', HomeController::class);
+Route::resource('/products', HomeController::class);//método que define rotas RESTful, incluem rotas para listagem, exibição, criação, edição, atualização, armazenamento e exclusão de produtos
 
 Route::get('/', function(){
     return view('main');
-});
+});//acesso a página inicial, quando acessa a página incial a view 'main' é retornada
 
